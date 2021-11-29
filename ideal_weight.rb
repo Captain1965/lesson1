5bfcdc7d2f7fbc2c371663be9c91c74eb9791717
Captain1965 <alex1965-03@mail.ru>
2021-11-29 23:43:22 +0300

    variant first

---
+++ ideal_weight.rb
@@ -0,0 +1,13 @@
+#encoding UTF-8
+#попросим пользователя последовательно ввести его имя user_name и его рост
+puts 'Введите ваше имя'
+user_name = gets.chomp
+puts 'Введите ваш рост в сантиметрах'
+height = gets.chomp.to_i
+	if (height - 110) * 1.15 < 0 
+		puts "#{user_name} Ваш вес уже оптимальный "
+	else
+	   puts "#{user_name} , Ваш идеальный вес #{(height - 110) * 1.15 }"  
+	end
+
+
